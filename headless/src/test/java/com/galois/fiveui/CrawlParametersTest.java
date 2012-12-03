3c6d1d27b578c8e5bffdf1f62fc26c7498df1a3b
Benjamin Jones <bjones@galois.com>
2012-12-03 13:17:28 -0800

    added CrawlParameters class and unit tests

---
+++ headless/src/test/java/com/galois/fiveui/CrawlParametersTest.java
@@ -0,0 +1,80 @@
+package com.galois.fiveui;
+
+import org.junit.Assert;
+import org.junit.Test;
+import com.galois.fiveui.CrawlParameters;
+
+public class CrawlParametersTest {
+
+	@Test
+	public void testConstructorFields() {
+		CrawlParameters c;
+		try {
+			c = new CrawlParameters("0 1 100 foo");
+		} catch (Exception e) {
+			Assert.assertTrue("failed to parse crawl type string", false);
+			return;
+		}
+		Assert.assertArrayEquals(new int[]{0, 1, 100}, new int[]{c.depth, c.maxFetch, c.politeness});
+		Assert.assertEquals("foo", c.match);
+	}
+	
+	@Test
+	public void testMatchFcn() {
+		CrawlParameters c;
+		try {
+			c = new CrawlParameters("0 1 100 foo");
+		} catch (Exception e) {
+			Assert.assertTrue("failed to parse crawl type string", false);
+			return;
+		}
+		Assert.assertTrue("failed to match foo", c.matchFcn.apply("foo"));
+		Assert.assertFalse("failed to not match bar", c.matchFcn.apply("bar"));
+	}
+	
+	@Test
+	public void testMatchFcnGlob1() {
+		CrawlParameters c;
+		try {
+			c = new CrawlParameters("0 1 100 foo*");
+		} catch (Exception e) {
+			Assert.assertTrue("failed to parse crawl type string", false);
+			return;
+		}
+		Assert.assertTrue("failed to match foo", c.matchFcn.apply("foo"));
+		Assert.assertFalse("failed to not match bar", c.matchFcn.apply("bar"));
+		Assert.assertTrue("failed to match foobar", c.matchFcn.apply("foobar"));
+		Assert.assertFalse("failed to not match barfoobar", c.matchFcn.apply("barfoobar"));
+	}
+	
+	@Test
+	public void testMatchFcnGlob2() {
+		CrawlParameters c;
+		try {
+			c = new CrawlParameters("0 1 100 http://foo.com/*.html");
+		} catch (Exception e) {
+			Assert.assertTrue("failed to parse crawl type string", false);
+			return;
+		}
+		Assert.assertTrue("failed to match http://foo.com/index.html", c.matchFcn.apply("http://foo.com/index.html"));
+		Assert.assertTrue("failed to match http://foo.com/test/index.html", c.matchFcn.apply("http://foo.com/test/index.html"));
+		Assert.assertFalse("failed to not match http://bar.com/index.html", c.matchFcn.apply("http://bar.com/index.html"));
+	}
+	
+	@Test
+	public void testMatchFcnGlob3() {
+		CrawlParameters c;
+		try {
+			c = new CrawlParameters("0 1 100 *foo.com*");
+		} catch (Exception e) {
+			Assert.assertTrue("failed to parse crawl type string", false);
+			return;
+		}
+		Assert.assertTrue("failed to match http://foo.com/index.html", c.matchFcn.apply("http://foo.com/index.html"));
+		Assert.assertTrue("failed to match http://foo.com/test/index.html", c.matchFcn.apply("http://foo.com/test/index.html"));
+		Assert.assertTrue("failed to match http://bar.foo.com", c.matchFcn.apply("http://bar.foo.com"));
+		Assert.assertFalse("failed to not match http://foobar.com", c.matchFcn.apply("http://foobar.com"));
+	}
+	
+}
+
