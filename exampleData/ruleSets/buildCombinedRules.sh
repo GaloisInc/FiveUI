c0f242cd6567bf7e7f883333b8831869452b28ac
Benjamin Jones <bjones@galois.com>
2012-12-07 17:32:44 -0800

    updates

--- exampleData/ruleSets/buildCombinedRules.sh
+++ exampleData/ruleSets/buildCombinedRules.sh
@@ -6,8 +6,11 @@
 # For the script to work, the input .json files must have rules separated by
 # comment lines starting with //---. Also, the last rule in the file must be
 # followed by //,. See colorRules.json for an example.
+#
+# Usage: buildCombinedRules <rule1.json> [<rule2.json> ...]
+#
 
-FILES=`ls *.json | grep -v combinedRules`
+FILES=$*
 OUTFILE="combinedRules.json"
 TMPFILE=`mktemp -t $0`
 HEADER="combinedHeader" # file containing header
