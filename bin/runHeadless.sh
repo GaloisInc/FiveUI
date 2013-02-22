3a846e0c73ed64a1737d8af9607ed99052171676
Benjamin Jones <bjones@galois.com>
2013-02-21 17:46:40 -0800

    moved FIVEUI_ROOT/bin to FIVEUI_ROOT/headless/bin and updated references in docs

--- bin/runHeadless.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-export FIVEUI_ROOT_PATH=$HOME/FiveUI
-export FIREFOX_BIN_PATH=/usr/bin/firefox
-
-java -DFIVEUI_ROOT_PATH=$FIVEUI_ROOT_PATH \
-     -DFIREFOX_BIN_PATH=$FIREFOX_BIN_PATH \
-     -jar $FIVEUI_ROOT_PATH/bin/HeadlessRunner-0.0.1-SNAPSHOT.one-jar.jar \
-     $*
