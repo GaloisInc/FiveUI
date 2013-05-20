3cfcb7f4bc40f9cae88d6cd871198a648b13a34c
Trevor Elliott <trevor@galois.com>
2013-05-20 11:31:34 -0700

    Flatten the jasmine tree

--- contexts/data/lib/jasmine/spec/SpecHelper.js
+++
@@ -1,9 +0,0 @@
-beforeEach(function() {
-  addMatchers({
-    toBePlaying: function(expectedSong) {
-      var player = this.actual;
-      return player.currentlyPlayingSong === expectedSong && 
-             player.isPlaying;
-    }
-  });
-});
