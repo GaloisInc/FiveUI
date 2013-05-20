dc598cd068e4d4ae9294c7b6183b9ff2826eea6c
Trevor Elliott <trevor@galois.com>
2013-05-20 12:37:55 -0700

    Fix a bug in the rules spec, and remove debug logging

--- contexts/data/tests/specs/rules.js
+++ contexts/data/tests/specs/rules.js
@@ -28,9 +28,7 @@ describe('fiveui.RuleSet', function() {
     var rule1 = new fiveui.Rule(42, 'r1', 'desc1', 'rule txt1');
     var rule2 = new fiveui.Rule(43, 'r2', 'desc2', 'rule txt2');
 
-    var ruleSet = new fiveui.RuleSet(42, 'rule set', 'desc', [rule1, rule2]);
-
-    console.log(ruleSet.dependencies);
+    var ruleSet = new fiveui.RuleSet(42, 'rule set', 'desc', [rule1, rule2], '');
 
     var jsonSet     = jQuery.toJSON(ruleSet);
     var restoredSet = fiveui.RuleSet.fromJSON(42, jQuery.secureEvalJSON(jsonSet));
@@ -38,7 +36,8 @@ describe('fiveui.RuleSet', function() {
     expect(restoredSet.id).toBe(ruleSet.id);
     expect(restoredSet.name).toBe(ruleSet.name);
     expect(restoredSet.description).toBe(ruleSet.description);
-    expect(restoredSet.rules.length).toBe(ruleSet.rules.length);
+    expect(restoredSet.rules).toEqual(ruleSet.rules);
+    expect(restoredSet.original).toEqual(ruleSet.original);
     expect(restoredSet.dependencies.length).toBe(0);
 
 
@@ -48,10 +47,9 @@ describe('fiveui.RuleSet', function() {
 
     var rule1 = new fiveui.Rule(42, 'r1', 'desc1', 'rule txt1');
     var rule2 = new fiveui.Rule(43, 'r2', 'desc2', 'rule txt2');
-    var deps  = ['dep1.js', 'dep2.js'];
 
     var ruleSet = new fiveui.RuleSet(42, 'rule set', 'desc', [rule1, rule2],
-        deps);
+        '', ['dep1.js', 'dep2.js']);
 
     var jsonSet     = jQuery.toJSON(ruleSet);
     var restoredSet = fiveui.RuleSet.fromJSON(42, jQuery.secureEvalJSON(jsonSet));
@@ -60,11 +58,7 @@ describe('fiveui.RuleSet', function() {
     expect(restoredSet.name).toBe(ruleSet.name);
     expect(restoredSet.description).toBe(ruleSet.description);
     expect(restoredSet.rules.length).toBe(ruleSet.rules.length);
-
-    _.each(restoredSet.dependencies, function(dep, ix) {
-      expect(dep).toBe(deps[ix]);
-    });
-
+    expect(restoredSet.dependencies).toEqual(ruleSet.dependencies);
   });
 
 });
