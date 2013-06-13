712f224acf5c8b80625597fca206b21159fa759e
Benjamin Jones <bjones@galois.com>
2013-06-13 15:21:37 -0700

    converted basicUIRules.json rulesets to new format

---
+++ exampleData/ruleSets/basic/emptyHrefs.js
@@ -0,0 +1,12 @@
+exports.name': "Don't use empty hrefs."
+exports.description': "Links with no text can't generally be used."
+exports.rule =
+  function() {
+    var that = this;
+    fiveui.query('a').each(
+      function(i, elt) {
+        if ($(elt).text() == '' && elt.title == '') {
+          that.report('Link has no text', elt);
+        }
+      });
+  };
