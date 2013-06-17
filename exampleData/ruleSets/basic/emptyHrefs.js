27e7d5dbd6d6510df67b72f193fbdaaf7e837e59
Benjamin Jones <bjones@galois.com>
2013-06-17 00:22:49 -0700

    big cleanup and reorganization of exampleData directory; bugfixes to the basic/ rules; added test_basic.html for testing basic UI rules

--- exampleData/ruleSets/basic/emptyHrefs.js
+++ exampleData/ruleSets/basic/emptyHrefs.js
@@ -1,12 +1,11 @@
 exports.name = "Don't use empty hrefs";
 exports.description = "Links with no text can't generally be used";
 exports.rule =
-  function() {
-    var that = this;
+  function(report) {
     fiveui.query('a').each(
       function(i, elt) {
         if ($(elt).text() == '' && elt.title == '') {
-          that.report('Link has no text', elt);
+          report.error('Link has no text', elt);
         }
       });
   };
--- exampleData/ruleSets/basic/labelFields.js
+++ exampleData/ruleSets/basic/labelFields.js
@@ -4,19 +4,18 @@ exports.description = '<p>Screen readers rely on HTML attributes to identify the
                      + 'attributes that specify the id of the form element they pertain to. '
                      + 'Some of the components of this web page do not have those labels.</p>';
 
-exports.rule = function() {
-  var that = this;
-  fiveui.query(':input').each(
+exports.rule = function(report) {
+  $5(':input').each(
     function(i, elt) {
       if (elt.id) {
-        var $label = fiveui.query("label[for='" + elt.id + "']");
+        var $label = $5("label[for='" + elt.id + "']");
 
         if (1 < $label.size()) {
-          that.report('Form element has too many labels', elt);
+          report.error('Form element has too many labels', elt);
         }
 
         if (0 == $label.size()) {
-          that.report('Form element has no label', elt);
+          report.error('Form element has no label', elt);
         }
       }
   });
