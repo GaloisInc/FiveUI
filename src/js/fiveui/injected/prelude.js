1040ff727ae8c4b6954a4093bbaaa41ddd2ae080
Jesse Hallett <jesse@galois.com>
2014-01-29 15:43:21 -0800

    Removes JSON.stringify implementation
    JSON is implemented natively in pretty much every browser these days:
    http://caniuse.com/#search=JSON
    
    That link only shows the five most recent versions of Firefox and
    Chrome.  Both have implemented JSON going much further back than Firefox
    25 or Chrome 31.

--- src/js/fiveui/injected/prelude.js
+++ src/js/fiveui/injected/prelude.js
@@ -602,28 +602,3 @@ fiveui.font.validate = function (allow, font) {
  * @param {?Node} node The node in the DOM that caused the problem.
  * @name report
  */
-
-/**
- * JSON.stringify is included here for extension and ruleset debugging
- * purposes. It is used by the jQuery pluggin jQuery.fn.log.
- */
-var JSON = JSON || {};
-JSON.stringify = JSON.stringify || function (obj) {
-    var t = typeof (obj);
-    if (t != "object" || obj === null) {
-        // simple data type
-        if (t == "string") obj = '"'+obj+'"';
-        return String(obj);
-    }
-    else {
-        // recurse array or object
-        var n, v, json = [], arr = (obj && obj.constructor == Array);
-        for (n in obj) {
-            v = obj[n]; t = typeof(v);
-            if (t == "string") v = '"'+v+'"';
-            else if (t == "object" && v !== null) v = JSON.stringify(v);
-            json.push((arr ? "" : '"' + n + '":') + String(v));
-        }
-        return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
-    }
-};
