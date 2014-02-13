3aab33978a5daa1797aa28368075f1cb80149cba
Jesse Hallett <jesse@sitr.us>
2014-02-12 17:07:50 -0800

    Initial commit of Internet Explorer plugin project

---
+++ src/js/internet_explorer/FiveUI/FiveUI/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+
+namespace FiveUI
+{
+    public partial class HighlighterOptionsForm : Form
+    {
+        public HighlighterOptionsForm()
+        {
+            InitializeComponent();
+        }
+
+        public string InputText
+        {
+            get { return this.textBox1.Text; }
+            set { this.textBox1.Text = value; }
+        }
+    }
+}
---
+++ src/js/internet_explorer/FiveUI/FiveUI/Interop.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace FiveUI
+{
+    [ComVisible(true)]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("80AE698C-1777-4986-B8CA-CDFABAB101EE")]
+    public interface IObjectWithSite
+    {
+        [PreserveSig]
+        int SetSite([MarshalAs(UnmanagedType.IUnknown)]object site);
+        [PreserveSig]
+        int GetSite(ref Guid guid, [MarshalAs(UnmanagedType.IUnknown)]out IntPtr ppvSite);
+    }
+
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct OLECMDTEXT
+    {
+        public uint cmdtextf;
+        public uint cwActual;
+        public uint cwBuf;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 100)]
+        public char rgwz;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct OLECMD
+    {
+        public uint cmdID;
+        public uint cmdf;
+    }
+
+    [ComImport(), ComVisible(true),
+    Guid("B722BCCB-4E68-101B-A2BC-00AA00404770"),
+    InterfaceTypeAttribute(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IOleCommandTarget
+    {
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        [PreserveSig]
+        int QueryStatus(
+            [In] IntPtr pguidCmdGroup,
+            [In, MarshalAs(UnmanagedType.U4)] uint cCmds,
+            [In, Out, MarshalAs(UnmanagedType.Struct)] ref OLECMD prgCmds,
+            //This parameter must be IntPtr, as it can be null
+            [In, Out] IntPtr pCmdText);
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        [PreserveSig]
+        int Exec(
+            //[In] ref Guid pguidCmdGroup,
+            //have to be IntPtr, since null values are unacceptable
+            //and null is used as default group!
+            [In] IntPtr pguidCmdGroup,
+            [In, MarshalAs(UnmanagedType.U4)] uint nCmdID,
+            [In, MarshalAs(UnmanagedType.U4)] uint nCmdexecopt,
+            [In] IntPtr pvaIn,
+            [In, Out] IntPtr pvaOut);
+    }
+}
