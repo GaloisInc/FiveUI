d92310fa4a756c7707fec165d05478a3c29cff77
Jesse Hallett <jesse@sitr.us>
2014-02-26 13:41:05 -0800

    Injects FiveUI scripts

--- src/js/internet-explorer/FiveUI/Injecter.cs
+++ src/js/internet-explorer/FiveUI/Injecter.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;  // provides MessageBox
@@ -17,20 +19,41 @@ namespace FiveUI
         private Regex urlPattern =
             new Regex(@"^http.*://.*\.wikipedia\.org/wiki/.*$", RegexOptions.IgnoreCase);
 
-        private Assembly assembly;
-        private StreamReader textStreamReader;
-
         public void execute(IWebBrowser2 browser)
         {
-            var manifestUrl = manifestForLocation(browser);
-            if (manifestUrl != null)
-            {
-                var ruleSet = RuleSet.Fetch(manifestUrl);
-                MessageBox.Show("RulesDir: "+ ruleSet.RulesDir);
+            Func<string, bool> isJs = s => s.EndsWith(".js");
+            Func<string, bool> run  = s => inject(browser, load(s));
+
+            computeScripts().Where(isJs).Select(run);
+            uiScripts().Where(isJs).Select(run);
 
                 //inject(browser, "alert('hello');");
                 //inject(browser, load("test.js"));
-            }
+        }
+
+        private List<string> computeScripts() {
+            var list = new List<string>();
+            list.Add("underscore.js");
+            list.Add("jquery/jquery-1.8.3.js");
+            list.Add("md5.js");
+            list.Add("injected/prelude.js");
+            list.Add("injected/compute.js");
+            return list;
+        }
+
+        private List<string> uiScripts() {
+            var list = new List<string>();
+            list.Add("underscore.js");
+            list.Add("font-awesome/css/font-awesome.css");
+            list.Add("css/ui.css");
+            list.Add("jquery/bundled.css");
+            list.Add("jquery/jquery-1.8.3.js");
+            list.Add("jquery/jquery-ui-1.9.2.custom.js");
+            list.Add("injected/injected.css");
+            list.Add("injected/prelude.js");
+            list.Add("injected/ui.js");
+            list.Add("injected/jquery-plugins.js");
+            return list;
         }
 
         private Uri manifestForLocation(IWebBrowser2 browser)
@@ -51,14 +74,16 @@ namespace FiveUI
 
         private string load(string script)
         {
-            assembly = Assembly.GetExecutingAssembly();
-            textStreamReader = new StreamReader(
-                    assembly.GetManifestResourceStream("FiveUI."+ script)
+            var scriptRef = "FiveUI.data." + script.Replace('/', '.');
+            var assembly = Assembly.GetExecutingAssembly();
+            MessageBox.Show("loading: "+ scriptRef);
+            var textStreamReader = new StreamReader(
+                    assembly.GetManifestResourceStream("FiveUI.data."+ script)
                     );
             return textStreamReader.ReadToEnd();
         }
 
-        private void inject(IWebBrowser2 browser, string code)
+        private bool inject(IWebBrowser2 browser, string code)
         {
             HTMLDocument document = (HTMLDocument) browser.Document;
             IHTMLElement head = (IHTMLElement)((IHTMLElementCollection)
@@ -71,6 +96,7 @@ namespace FiveUI
             ((HTMLHeadElement)head).appendChild((IHTMLDOMNode)scriptObject);
             // TODO: can I remove after script is processed?
             ((HTMLHeadElement)head).removeChild((IHTMLDOMNode)scriptObject);
+            return true;
         }
 
     }
