e3f759b44ce2e5f7e51939837d021de7a8bf4779
Rogan Creswick <creswick@gmail.com>
2014-03-16 12:03:04 -0700

    added tests for imageAlt, fixed bugs in the rule

--- guidelines/wikipedia/specs/imageAlt_spec.js
+++ guidelines/wikipedia/specs/imageAlt_spec.js
@@ -3,7 +3,12 @@ describe('imageAlt', function() {
 
   afterEach(teardownFixtures);
 
-  it('succeeds', function() {
-    expect(true).toBe(true);
+  it('Warns of img tags that lack an alt attribute', function(){
+    fixture('<img src="noalt.jpg"/>');
+    fixture('<img src="emptyalt.jpg" alt=""/>');
+    fixture('<img src="somealt.jpg" alt="alt text"/>');
+
+    var results = run(imageAlt);
+    expect(results.warnings.length).toEqual(2);
   });
 });
--- guidelines/wikipedia/src/imageAlt.js
+++ guidelines/wikipedia/src/imageAlt.js
@@ -1,8 +1,14 @@
 exports.name = "Images should include an alt attribute";
-exports.description = "Images should include an alt attribute, even an empty one, that acts as a substitute for the image (for non-visual users).";
+exports.description = "Images should include an alt attribute that acts as a substitute for the image (for non-visual users).";
 
 exports.rule = function(report) {
-  $5('#mw-content-text img:not([alt])').each(function(i, img) {
-    report.warning("Image has no alt attribute.", img);
-  });
+  var hasAlt = function(ix) {
+    var alt = $.trim($(this).attr('alt'));
+    var longdesc = $.trim($(this).attr('longdesc'));
+    if(_.isEmpty(alt) && _.isEmpty(longdesc)) {
+      report.warning('No alt/longdesc specified', this);
+    }
+  };
+
+  $5('#mw-content-text').find('img').each(hasAlt);
 };
