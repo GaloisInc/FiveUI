a57f253c470ffe6bb0868cd7eeac9fbc95f06a55
Rogan Creswick <creswick@gmail.com>
2014-04-02 17:24:38 -0700

    added skeleton class for a settings button

---
+++ src/js/internet-explorer/FiveUI/SettingsButton.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using mshtml;
+using SHDocVw;
+
+
+namespace FiveUI
+{
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+    [Guid("42BB1432-CEED-40FB-AD94-C38FC5EA78A3")]
+    [ProgId("Galois.FiveUISettings")]
+    class SettingsButton : IObjectWithSite, IOleCommandTarget
+    {
+        private IWebBrowser2 browser;
+
+        [Guid("22B31783-5E42-4D09-88AF-6F8790662E61")]
+        [InterfaceType(1)]
+        public interface IServiceProvider
+        {
+            int QueryService(ref Guid guidService, ref Guid riid, out IntPtr ppvObject);
+        }
+
+        #region Implementation of IObjectWithSite
+        int IObjectWithSite.SetSite(object site)
+        {
+            IServiceProvider serviceProvider = (IServiceProvider)site;
+
+            if (serviceProvider != null)
+            {
+                var guidIWebBrowserApp = Marshal.GenerateGuidForType(typeof(IWebBrowserApp)); // new Guid("0002DF05-0000-0000-C000-000000000046");
+                var guidIWebBrowser2 = Marshal.GenerateGuidForType(typeof(IWebBrowser2)); // new Guid("D30C1661-CDAF-11D0-8A3E-00C04FC9E26E");
+                IntPtr intPtr;
+                serviceProvider.QueryService(ref guidIWebBrowserApp, ref guidIWebBrowser2, out intPtr);
+
+                browser = (IWebBrowser2)Marshal.GetObjectForIUnknown(intPtr);
+            }
+            else
+            {
+                browser = null;
+            }
+            return 0;
+        }
+
+        int IObjectWithSite.GetSite(ref Guid guid, out IntPtr ppvSite)
+        {
+            System.Diagnostics.Debugger.Break();
+            IntPtr punk = Marshal.GetIUnknownForObject(browser);
+            int hr = Marshal.QueryInterface(punk, ref guid, out ppvSite);
+            Marshal.Release(punk);
+            return hr;
+        }
+        #endregion
+
+        #region Implementation of IOleCommandTarget
+        int IOleCommandTarget.QueryStatus(IntPtr pguidCmdGroup, uint cCmds, ref OLECMD prgCmds, IntPtr pCmdText)
+        {
+            return 0;
+        }
+
+        /**
+         * Invoked when the button is clicked. 
+         */
+        int IOleCommandTarget.Exec(IntPtr pguidCmdGroup, uint nCmdID, uint nCmdexecopt, IntPtr pvaIn, IntPtr pvaOut)
+        {
+
+            if (browser == null)
+            {
+                MessageBox.Show("Error loading Settings page for FiveUI.");
+            }
+            else
+            {
+                MessageBox.Show("This should load the FiveUI Settings.");
+            }
+            return 0;
+        }
+        #endregion
+
+        #region Registering with regasm
+
+        [ComRegisterFunction]
+        public static void RegisterBHO(Type type)
+        {
+            string guid = type.GUID.ToString("B");
+
+            // BHO
+            {
+                RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(IEAddon.RegBHO, true);
+                if (registryKey == null)
+                    registryKey = Registry.LocalMachine.CreateSubKey(IEAddon.RegBHO);
+                RegistryKey key = registryKey.OpenSubKey(guid);
+                if (key == null)
+                    key = registryKey.CreateSubKey(guid);
+                key.SetValue("Alright", 1);
+                registryKey.Close();
+                key.Close();
+            }
+
+            // Command
+            {
+                RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(IEAddon.RegCmd, true);
+                if (registryKey == null)
+                    registryKey = Registry.LocalMachine.CreateSubKey(IEAddon.RegCmd);
+                RegistryKey key = registryKey.OpenSubKey(guid);
+                if (key == null)
+                    key = registryKey.CreateSubKey(guid);
+                key.SetValue("ButtonText", "FiveUI Settings");
+                key.SetValue("CLSID", "{1FBA04EE-3024-11d2-8F1F-0000F87ABD16}");
+                key.SetValue("ClsidExtension", guid);
+                key.SetValue("Icon", "");
+                key.SetValue("HotIcon", "");
+                key.SetValue("Default Visible", "Yes");
+                key.SetValue("MenuText", "&FiveUI Settings");
+                key.SetValue("ToolTip", "FiveUI Settings");
+                //key.SetValue("KeyPath", "no");
+                registryKey.Close();
+                key.Close();
+            }
+        }
+
+        [ComUnregisterFunction]
+        public static void UnregisterBHO(Type type)
+        {
+            string guid = type.GUID.ToString("B");
+            // BHO
+            {
+                RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(IEAddon.RegBHO, true);
+                if (registryKey != null)
+                    registryKey.DeleteSubKey(guid, false);
+            }
+            // Command
+            {
+                RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(IEAddon.RegCmd, true);
+                if (registryKey != null)
+                    registryKey.DeleteSubKey(guid, false);
+            }
+        }
+        #endregion
+    }
+}
