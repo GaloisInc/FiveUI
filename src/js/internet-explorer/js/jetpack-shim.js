481d3260edfe679415823958890cdeea4cdf213b
Jesse Hallett <jesse@sitr.us>
2014-04-09 19:08:54 -0700

    Fixes StorageWrapper

--- src/js/internet-explorer/js/jetpack-shim.js
+++ src/js/internet-explorer/js/jetpack-shim.js
@@ -166,8 +166,19 @@
 
   /* StorageWrapper */
 
-  var store = _fiveui_store;
   function StorageWrapper() {}
-  $.extend(StorageWrapper.prototype, store);
+  $.extend(StorageWrapper.prototype, {
+    key: function(idx) { return _fiveui_store.key(idx); },
+    getItem: function(key) { return _fiveui_store.getItem(key); },
+    setItem: function(key, val) { _fiveui_store.setItem(key, val); },
+    removeItem: function(key) { _fiveui_store.removeItem(key); },
+    clear: function() { _fiveui_store.clear(); },
+  });
+  Object.defineProperty(StorageWrapper.prototype, 'length', {
+    get: function() {
+      return _fiveui_store.size();
+    },
+    enumerable: false
+  });
 
 }(window, jQuery));
