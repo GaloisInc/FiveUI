9d95b93601d293cc398b0158efe39491b3753517
Rogan Creswick <creswick@gmail.com>
2014-04-23 14:34:16 -0700

    final tweaks to the bullets rule

--- guidelines/wikipedia/src/bullets.js
+++ guidelines/wikipedia/src/bullets.js
@@ -1,10 +1,12 @@
 exports.name = "Minimize use of bullet points";
 exports.description = "Bullet points should be minimized in the body and lead of the article, if they are used at all.";
 
-exports.ratio = 0.5;
-exports.special_section_ids =  [ 'Notes',
-                                 'Related_links',
-                                 'External_links'];
+ratio = 0.5;
+special_section_ids =  [ 'Notes',
+                         'Related links',
+                         'External links',
+                         'See also',
+                         'References'];
 
 exports.rule = function(report) {
   var ss = sections('#mw-content-text');
@@ -20,13 +22,12 @@ exports.rule = function(report) {
   function check_bullets(section) {
     var bullet_txt = section.filter('ul, ol').text();
 
-
     var all_txt = section.text();
     var all_chars = all_txt.length;
     var bullet_chars = bullet_txt.length;
 
     if (0 !== all_chars) {
-      if (bullet_chars / all_chars >= exports.ratio) {
+      if (bullet_chars / all_chars >= ratio) {
         var msg = "The "+section_name(section)+" section uses too many bullet points.";
         report.warning(msg, section);
       }
@@ -34,10 +35,10 @@ exports.rule = function(report) {
   }
 
   function not_special_section(section) {
-    var id = section.attr('id');
-    var isSpecial = _.contains(exports.special_section_ids, id);
+      var id = section_name(section);
+      var isSpecial = _.contains(special_section_ids, id);
 
-    return ! isSpecial;
+      return ! isSpecial;
   }
 };
 
